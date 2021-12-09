3dfc131dafa860d7494ef9df863ef54168b3ae6f
Jabrail Chumakov <67557966+jabrail-chumakov@users.noreply.github.com>
2021-12-09 17:11:01 +0500

    Update run.m

--- epipolar_lines/run.m
+++ epipolar_lines/run.m
@@ -1,18 +1,20 @@
-% % Load necessary pictures and points
-Im1 = imread('/Rubik/Rubik1.pgm');
-Im2 = imread('/Rubik/Rubik2.pgm');
-pt1 = importdata('~/Rubik/Rubik1.points');
-pt2 = importdata('~/Rubik/Rubik2.points');
+%% Load necessary pictures and points for Rubik
+Im1 = imread('Rubik/Rubik1.pgm');
+Im2 = imread('Rubik/Rubik2.pgm');
+pt1 = importdata('Rubik/Rubik1.points');
+pt2 = importdata('Rubik/Rubik2.points');
 
-% % Mire 
-% Im1 = imread('/Mire/Mire1.pgm');
-% Im2 = imread('/Mire/Mire2.pgm');
-% pt1 = importdata('/Mire/Mire1.points');
-% pt2 = importdata('/Mire/Mire1.points');
+%% Load necessary pictures and points for Mire
+% Im1 = imread('Mire/Mire1.pgm');
+% Im2 = imread('Mire/Mire2.pgm');
+% pt1 = importdata('Mire/Mire1.points');
+% pt2 = importdata('Mire/Mire1.points');
 
-% % Version one
+%% Version one without Normalization
 F = EightPointsAlgorithm(pt1, pt2);
-% % Version two
+
+%% Version one with Normalization
 % F = EightPointsAlgorithmN(pt1, pt2);
-% % Visualise epipolar lines
+
+%% Visualise epipolar lines
 visualizeEpipolarLines(Im1, Im2, F, pt1, pt2)
